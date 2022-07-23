423a3a36befdb190bebab1ca82084ac68a4f5bad
Atevaz <101355785+Atevaz@users.noreply.github.com>
2022-07-23 17:46:45 +0200

    app bar and category and drawer

--- lib/presentation/router/app_router.dart
+++ lib/presentation/router/app_router.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ndoora_shopping/presentation/screens/shared/home_layout/home_layout_screen.dart';
 import '../screens/user/home_screen.dart';
 import 'app_router_names.dart';
 
@@ -6,7 +7,7 @@ class AppRouter {
   Route? onGenerateRoute(RouteSettings settings) {
     switch (settings.name) {
       case AppRouterNames.rSplashRoute:
-        return MaterialPageRoute(builder: (_) => HomeScreen());
+        return MaterialPageRoute(builder: (_) =>HomeLayoutScreen());
       default:
         return null;
     }
